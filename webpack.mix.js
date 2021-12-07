fd434db05b3454093df0ff03d364c9880fd0ccff
Malinda Mosholder <m.m.mosholder@gmail.com>
2021-12-07 10:28:42 -0500

    remove built files, add social share, favicons

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,11 +2,12 @@ let mix = require('laravel-mix');
 
 mix.js('src/js/app.js', 'dist/js')
     .sass('src/scss/style.scss', 'dist/css').options({
-    processCssUrls: false
-  })
-  .options({
-    postCss: [
-        require('postcss-preset-env')
-    ]
-})
+        processCssUrls: false
+    })
+    .options({
+        postCss: [
+            require('postcss-preset-env')
+        ]
+    })
+    .copy('src/images/', 'dist/images')
     .copy('src/fonts/', 'dist/fonts');
